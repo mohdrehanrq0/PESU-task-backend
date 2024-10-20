a640472f2b9f090a8c593293f9fcb82dbe848b99
Mohammad Rehan Qureshi <53863785+mohdrehanrq0@users.noreply.github.com>
2024-10-20 20:14:59 +0530

    fix cookie wala issue

--- src/modules/user/controller.ts
+++ src/modules/user/controller.ts
@@ -7,7 +7,7 @@ import catchAsyncError from '../../middleware/catchAsyncError';
 import User from '../../model/user';
 import ErrorHandler from '../../utils/errorHandler';
 import { getJWTtoken } from '../../utils/jwt';
-import { comparePassword, hashPassword } from './utils';
+import { comparePassword, cookieConfig, hashPassword } from './utils';
 
 export const registerUser = catchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -51,11 +51,7 @@ export const userLogin = catchAsyncError(
 
     const accessToken = await getJWTtoken(userData._id);
 
-    res.cookie("accessToken", accessToken, {
-      httpOnly: true,
-      secure: false,
-      maxAge: 90 * 24 * 3600 * 1000,
-    });
+    res.cookie("accessToken", accessToken, cookieConfig);
     res.status(statusCode.SUCCESS).json({
       success: true,
       message: "User login successfully.",
@@ -66,11 +62,7 @@ export const userLogin = catchAsyncError(
 
 export const userLogout = catchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
-    res.clearCookie("accessToken", {
-      httpOnly: true,
-      secure: false,
-      maxAge: 90 * 24 * 3600 * 1000,
-    });
+    res.clearCookie("accessToken", cookieConfig);
 
     res.status(statusCode.SUCCESS).json({
       success: true,
--- src/modules/user/utils.ts
+++ src/modules/user/utils.ts
@@ -1,4 +1,6 @@
-import bcrypt from "bcryptjs";
+import bcrypt from 'bcryptjs';
+
+import { ICookieConfig } from './interface';
 
 export const hashPassword = (password: string) => {
   return bcrypt.hashSync(password, 10);
@@ -7,3 +9,15 @@ export const hashPassword = (password: string) => {
 export const comparePassword = (password: string, hashPassword: string) => {
   return bcrypt.compareSync(password, hashPassword);
 };
+
+export const cookieExpiry = 90 * 24 * 3600 * 1000;
+
+export const cookieConfig: ICookieConfig =
+  process.env.NODE_ENV === "local"
+    ? { httpOnly: true, secure: false, maxAge: cookieExpiry }
+    : {
+        httpOnly: true,
+        secure: true,
+        maxAge: cookieExpiry,
+        sameSite: "none",
+      };
